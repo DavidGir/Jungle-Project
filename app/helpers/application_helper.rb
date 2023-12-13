abd84a1210c7ba738a2a3e2378e324c1469f1436
David Giroux <DavidGiroux149@gmail.com>
2023-12-13 17:31:50 -0500

    add application helper method to format prices in $; avoids hardcoding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # For uniformity throughout the app; a helper method to format the price could be useful
+  # This method will convert numeric values to a formatted string with a dollar sign
+  def formatted_price(price)
+    number_to_currency(price, unit: "$", precision: 2)
+  end
+
 end
