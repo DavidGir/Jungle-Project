17fc2c3a9c233abb0d34ec507ea265c400b51545
David Giroux <DavidGiroux149@gmail.com>
2023-12-15 01:13:44 -0500

    add routes for signup, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  
   namespace :admin do
     get 'categories/index'
     get 'categories/new'
