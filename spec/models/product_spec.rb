349574e8cc1005fab7d104bfa8936d904867e872
David Giroux <DavidGiroux149@gmail.com>
2023-12-18 20:20:06 -0500

    add test for price_cents

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,14 +6,20 @@ RSpec.describe Product, type: :model do
     # Validation test for name 
     it 'does not save without a name' do
       @category = Category.new
-      @product = Product.new(name: nil, price: 100, quantity: 10, category: @category)
+      @product = Product.new(name: nil, price_cents: 100, quantity: 10, category: @category)
 
       expect(@product.save).to be false
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     #Validation for price
-    
+    it 'does not save without a price' do
+      @category = Category.new
+      @product = Product.new(name: "Test Product", price_cents: nil, quantity: 10, category: @category)
+
+      expect(@product.save).to be false
+      expect(@product.errors.full_messages).to include("Price cents can't be blank")
+    end
     #Validation for quantity
 
     #Validation for category
