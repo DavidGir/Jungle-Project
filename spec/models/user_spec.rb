418ead7ccc0e7975d3d3c97e32f17efdc29088a0
David Giroux <DavidGiroux149@gmail.com>
2023-12-18 21:01:18 -0500

    add validation test for when password and password confirmation match

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+     # Test for presence of password and password_confirmation and their match
+     it 'validates presence of password and password_confirmation and their match' do
+      user = User.new(
+        first_name: 'Jo',
+        last_name: 'Doe',
+        email: 'jo@example.com',
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(user).to be_valid
+      
+      user.password_confirmation = 'different_password'
+      expect(user).to_not be_valid
+    end
+
+
+  end
+
+end
+
