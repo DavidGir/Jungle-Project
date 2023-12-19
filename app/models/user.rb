af5c66d797b8e722175432eb83cfe3a20ab52d6a
David Giroux <DavidGiroux149@gmail.com>
2023-12-18 21:47:50 -0500

    add class method for authentication with credentials for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  # Class method which takes into account edge cases
+  def self.authenticate_with_credentials(email, password)
+    # email.strip.downcase trims any whitespace and converts the email to lowercase.
+    # find_by('LOWER(email) = ?') finds the user in a case-insensitive manner.
+    user = User.find_by('LOWER(email) = ?', email.strip.downcase)
+    user && user.authenticate(password) ? user : nil
+  end
 end
