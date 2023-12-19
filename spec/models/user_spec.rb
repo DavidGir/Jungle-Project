55dd147c8a217e6676d3bee52aa155beb613bddc
David Giroux <DavidGiroux149@gmail.com>
2023-12-18 21:09:15 -0500

    add validation test to ensure that does not save without passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,23 @@ RSpec.describe User, type: :model do
         password_confirmation: '123'
       )
       expect(user).to be_valid
-      
+
       user.password_confirmation = 'different_password'
       expect(user).to_not be_valid
     end
+    # Test for password and password_confirmation
+    it 'does not save without a password and password_confirmation' do
+      user = User.new(
+        first_name: 'Jo',
+        last_name: 'Doe',
+        email: 'jo@example.com',
+        password: nil,
+        password_confirmation: nil
+      ) 
+      expect(user).to_not be_valid
+    end
 
-
+    
   end
 
 end
