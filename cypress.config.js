90502ca27b6b42857d241ada75a364ed41b1d1ac
David Giroux <DavidGiroux149@gmail.com>
2023-12-19 19:26:43 -0500

    add config for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
+    baseUrl: 'http://0.0.0.0:3000', 
+    specPattern: 'cypress/integration/**/*.spec.js',
+
     setupNodeEvents(on, config) {
+      // setupNodeEvents configuration
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        /* ... */
-      })
+        // Configuration or actions to take before the browser launches
+      });
     },
   },
   screenshotsFolder: "tmp/cypress_screenshots",
   videosFolder: "tmp/cypress_videos",
   trashAssetsBeforeRuns: false
-})
+});
